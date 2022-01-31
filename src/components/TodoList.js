05c100f30b34177d2d0a83eb178512758f1c4545
crushd <44733457+crushd@users.noreply.github.com>
2022-01-31 10:47:56 -0800

    added icons and created item remove function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -13,10 +14,32 @@ function TodoList() {
     console.log(todo, ...todos);
   };
 
+  const removeTodo = (id) => {
+    //checking the array
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    //remove from array
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        // toggle between true and false
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
     <Fragment>
       <h1>Today's Tasks</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodos={completeTodo}
+        removeTodo={removeTodo}
+      />
     </Fragment>
   );
 }
